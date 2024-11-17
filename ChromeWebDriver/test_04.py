a22c24ee92bec11911edfdff9ac1e17d4ff5aba1
Jafar Loka <joyhess501@gmail.com>
2024-11-17 10:11:42 +0200

    Changing The Test_04.py-File

--- ChromeWebDriver/test_04.py
+++ ChromeWebDriver/test_04.py
@@ -7,6 +7,8 @@ from selenium.webdriver.common.by import By;
 from selenium.webdriver.support.ui import WebDriverWait;
 from selenium.webdriver.support import expected_conditions as EC;
 
+import time;
+
 service = Service(executable_path='chromedriver.exe');
 
 driver = webdriver.Chrome(service=service);
@@ -24,6 +26,28 @@ login_element = driver.find_element(By.XPATH, '//a[@href="/login"]');
 print("The Login Element IS: ", login_element);
 
 login_element.click();
+
+print("The Current Page URL Is: ", driver.current_url);
+
+WebDriverWait(driver=driver, timeout=15).until(
+    EC.presence_of_element_located((By.XPATH, '//input[@id="email"]'))
+)
+
+email_input = driver.find_element(By.XPATH, '//input[@id="email"]');
+
+password_input = driver.find_element(By.XPATH, '//input[@id="login-password"]');
+
+print("The Email Input Is: ", email_input);
+print("The Password Input Is: ", password_input);
+
+email_input.send_keys("gaafer@loka.com");
+password_input.send_keys("Test@1234567890");
+
+login_btn = driver.find_element(By.XPATH, '//button[@id="login"]');
+
+login_btn.click();
+
+time.sleep(10);
 
 print("The Current Page URL Is: ", driver.current_url);
 
