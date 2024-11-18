c5cd24908f0895b4b3a409eaa63fcc78b2820af3
Jafar Loka <joyhess501@gmail.com>
2024-11-18 09:57:34 +0200

    Adding New File For CheckBoxes And Radio BTNs

--- ChromeWebDriver/test_05.py
+++ ChromeWebDriver/test_05.py
@@ -15,6 +15,8 @@ service = Service(executable_path="chromedriver.exe");
 
 driver = webdriver.Chrome(service=service);
 
+driver.maximize_window();
+
 driver.get("https://google.com/");
 
 search_element = driver.find_element(By.XPATH, "//textarea[@id='APjFqb']");
@@ -24,6 +26,10 @@ search_element.send_keys("Testing Using Selenium And BS4");
 img_element = driver.find_element(By.XPATH, "//img[@class='lnXdpd']");
 
 img_element.click();
+
+# img_element.is_displayed();
+# img_element.is_enabled();
+# img_element.is_selected();
 
 WebDriverWait(driver=driver, timeout=15).until(
     EC.presence_of_element_located((By.XPATH, "//input[@name='btnK']"))
---
+++ ChromeWebDriver/test_06.py
@@ -0,0 +1,37 @@
+from selenium import webdriver;
+
+from selenium.webdriver.chrome.service import Service;
+
+from selenium.webdriver.support import expected_conditions as EC;
+
+from selenium.webdriver.common.by import By;
+
+import time;
+
+service = Service(executable_path="chromedriver.exe");
+
+driver = webdriver.Chrome(service=service);
+
+driver.maximize_window();
+
+driver.get("https://www.letskodeit.com/practice");
+
+btn1 = driver.find_element(By.XPATH, "//input[@id='bmwradio']");
+
+btn1.click();
+
+btn2 = driver.find_element(By.XPATH, "//input[@id='bmwcheck']");
+
+btn2.click();
+
+btn3 = driver.find_element(By.XPATH, "//input[@id='hondacheck']");
+
+btn3.click();
+
+print("The State Of BTN 1 Is: ", btn1.is_selected());
+
+print("The State Of BTN 2 Is: ", btn2.is_selected());
+
+print("The State Of BTN 3 Is: ", btn3.is_selected());
+
+time.sleep(3);
