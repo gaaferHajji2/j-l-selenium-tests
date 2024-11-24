5e5b663d971669a42fa4135f016c58ea768b3dfa
Jafar Loka <joyhess501@gmail.com>
2024-11-24 10:36:27 +0300

    Handle Select Element

---
+++ ChromeWebDriver/test_07.py
@@ -0,0 +1,50 @@
+from selenium import webdriver;
+
+from selenium.webdriver.chrome.service import Service;
+
+from selenium.webdriver.common.by import By;
+
+from selenium.webdriver.support.ui import WebDriverWait;
+
+from selenium.webdriver.support import expected_conditions as EC;
+from selenium.webdriver.support.select import Select;
+
+import time;
+
+service = Service(executable_path='chromedriver.exe');
+
+driver = webdriver.Chrome(service=service);
+
+driver.maximize_window();
+
+driver.get("https://www.letskodeit.com/practice");
+
+WebDriverWait(driver=driver, timeout=15).until(
+    EC.presence_of_element_located((By.XPATH, '//select[@id="carselect"]'))
+);
+
+element = driver.find_element(By.XPATH, '//select[@id="carselect"]');
+
+print("Select Element Of Car List Is: ", element);
+
+time.sleep(2);
+
+select_element = Select(element);
+
+select_element.select_by_value('honda');
+print("Select By Value: bmw");
+
+time.sleep(1);
+
+# The Index Start From Zero.
+select_element.select_by_index(1);
+print("Select By Index: 1")
+
+time.sleep(1);
+
+select_element.select_by_visible_text('BMW');
+print("Select By Visible Text")
+
+time.sleep(1);
+
+driver.quit();
