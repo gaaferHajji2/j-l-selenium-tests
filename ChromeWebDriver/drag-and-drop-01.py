692d9def2b02e3ed807d933504279b390d8d25b5
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-01-13 14:41:46 +0300

    Test Drag And Drop In Two-Ways

---
+++ ChromeWebDriver/drag-and-drop-01.py
@@ -0,0 +1,50 @@
+from selenium import webdriver
+
+from selenium.webdriver import ActionChains
+
+from selenium.webdriver.chrome.service import Service
+
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.support.ui import WebDriverWait
+
+from selenium.webdriver.support import expected_conditions as EC;
+
+import time
+
+service = Service(executable_path='chromedriver.exe')
+
+driver = webdriver.Chrome(service=service)
+
+driver.get('https://jqueryui.com/draggable/')
+
+w1 = driver.current_window_handle
+
+w2 = driver.find_element(By.XPATH, '//iframe[@class="demo-frame"]');
+
+driver.switch_to.frame(frame_reference=w2);
+
+t1 = WebDriverWait(
+    driver=driver, 
+    timeout=15, 
+    poll_frequency=2.0
+).until(
+    EC.presence_of_element_located(
+        (By.XPATH, '//div[@id="draggable"]')
+    )
+)
+
+# t1 = driver.find_element(By.XPATH, '//div[@id="draggable"]');
+
+try:
+    actionChains = ActionChains(driver=driver);
+
+    actionChains.drag_and_drop_by_offset(source=t1, xoffset=50, yoffset=50).perform();
+
+    # actionChains.drag_and_drop(source, target);
+
+    time.sleep(3);
+
+    driver.switch_to.window(window_name=w1);
+except Exception as e:
+    print("The Exception Is: ", e.__str__());
