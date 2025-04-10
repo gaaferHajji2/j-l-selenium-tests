2e0ceeed89f72b36b7d62f1bb7e926ede755fc80
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-04-10 12:56:05 +0300

    Run First Test With SetUp And Teardown With Verbosity 3

--- tests/test_01.py
+++ tests/test_01.py
@@ -9,4 +9,10 @@ class Test01(unittest.TestCase):
         print("The TearDown Method Of Test01-Class TestCase")
 
     def test_method_01(self):
-        pass
+        print("Running From Method-01");
+
+    def test_method_02(self):
+        print("Running From Method-02");
+
+if __name__ == '__main__':
+    unittest.main(verbosity=3)
