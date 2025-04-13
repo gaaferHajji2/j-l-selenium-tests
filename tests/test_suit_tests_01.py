9619d56f9d254c0cac95899d1181acb391d91341
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-04-13 11:33:39 +0300

    Finishing From Building Test Suite With TextTestRunner

--- tests/test_suit_tests_01.py
+++ tests/test_suit_tests_01.py
@@ -1,6 +1,16 @@
 import unittest
 
-from tests.test_01 import Test01
-from tests.test_02 import Test02
-from tests.test_assertion_01 import TestAssertion
+from test_01 import Test01
+from test_02 import Test02
+from test_assertion_01 import TestAssertion
 
+# Getting All Tests From The Classes That Inherit From Test01, Test02, TestAssertion
+t1 = unittest.TestLoader().loadTestsFromTestCase(Test01)
+t2 = unittest.TestLoader().loadTestsFromTestCase(Test02)
+t3 = unittest.TestLoader().loadTestsFromTestCase(TestAssertion)
+
+# Create The Test Suite That Contains All Tests Cases
+t4 = unittest.TestSuite([t1, t2, t3])
+
+# Run The Tests With Verbosity Of 2
+t5 = unittest.TextTestRunner(verbosity=3).run(t4)
