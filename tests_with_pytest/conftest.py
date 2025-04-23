dbabc025e207a810f057df8542cfab6a9390e8e2
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-04-23 11:24:29 +0300

    Test Ordering Using Markers In Pytest

--- tests_with_pytest/conftest.py
+++ tests_with_pytest/conftest.py
@@ -7,3 +7,9 @@ def simple_01():
 @pytest.fixture(scope='module')
 def simple_02():
     print("In The Fixture Simple_02 With Scope: Module")
+
+@pytest.fixture(autouse=True, scope='session')
+def fixture02():
+    print("This Will Run Automatically Before Any Test");
+    yield
+    print("This Will Run Automatically After Any Test Because Of Scope Is: Session");
--- tests_with_pytest/test_fixture_01.py
+++ tests_with_pytest/test_fixture_01.py
@@ -5,17 +5,19 @@ import pytest
 # def fixture01():
 #     print("This Will Run Before The Test That Used It")
 
-@pytest.fixture(autouse=True)
-def fixture02():
-    print("This Will Run Automatically Before Any Test");
-    yield
-    print("This Will Run Automatically After Any Test Because Of Scope Is: Function");
+@pytest.mark.run(order=2)
+def test_02():
+    print("The Test Is 02")
+    
+    assert True == True
 
+@pytest.mark.run(order=3)
+def test_03():
+    print("The Test-03 Test")
+
+    assert True
+
+@pytest.mark.run(order=1)
 def test_01():
     print("The Test-01 Test");
     assert 1 == 1
-
-def test_02():
-    print("The Test-02 Test")
-
-    assert True
