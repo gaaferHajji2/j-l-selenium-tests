eb3ed8e14babf29eacf145e0a1e1d269d121aa50
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-07-01 15:00:30 +0300

    Refactor code and build get driver method

---
+++ Page_Object_Model_POM/utils/driver.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+
+from selenium.webdriver.chrome.service import Service
+
+import pytest
+
+@pytest.fixture()
+def get_chrome_driver():
+    service = Service(executable_path='./chromedriver.exe');
+
+    driver = webdriver.Chrome(service=service);
+
+    return driver;
