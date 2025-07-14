f935cec97e7aafc496d297a5deeafd15d8646e6d
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-07-14 13:49:42 +0300

    Adding first assert for login url

--- Page_Object_Model_POM/tests/test_login_page.py
+++ Page_Object_Model_POM/tests/test_login_page.py
@@ -8,6 +8,8 @@ def test_login_page(get_driver_and_web_wait):
     loginPage.click_login_link()
     print("The current URL Is: ", loginPage.return_url());
 
+    assert loginPage.return_url() == "https://www.letskodeit.com/login"
+
     loginPage.enter_email_addr()
     loginPage.enter_password()
     loginPage.click_login_btn()
