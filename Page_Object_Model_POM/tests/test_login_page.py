cd883dccde75d35b6bb082327b626a34293d0660
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-07-14 13:48:14 +0300

    fix bug about loading modules by adding pytest.ini and build login page test

---
+++ Page_Object_Model_POM/tests/test_login_page.py
@@ -0,0 +1,13 @@
+import ui_pages_classes.login_page
+
+def test_login_page(get_driver_and_web_wait):
+    driver, wait = get_driver_and_web_wait
+
+    loginPage = ui_pages_classes.login_page.LoginPage(driver=driver, wait=wait)
+
+    loginPage.click_login_link()
+    print("The current URL Is: ", loginPage.return_url());
+
+    loginPage.enter_email_addr()
+    loginPage.enter_password()
+    loginPage.click_login_btn()
